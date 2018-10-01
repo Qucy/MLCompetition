527971014409fc41d09af72140beab1cb65b4753
Qucy <tracyqucy@gmail.com>
2018-10-01 22:35:20 +0800

    update sentiment analysis

--- AIChallenger/sentiment_analysis/baseline/config.py
+++ AIChallenger/sentiment_analysis/baseline/config.py
@@ -3,9 +3,11 @@
 
 import os
 model_save_path = os.path.abspath('..') + "/data/"
-train_data_path = "训练集文件存放路径"
-validate_data_path = "验证集文件存放路径"
-test_data_path = "测试集文件存放路径"
-test_data_predict_out_path = "测试集预测结果文件存放路径"
-
-
+# 训练集文件存放路径
+train_data_path = "../data/training/sentiment_analysis_trainingset.csv"
+# 验证集文件存放路径
+validate_data_path = "../data/validation/sentiment_analysis_validationset.csv"
+# 测试集文件存放路径
+test_data_path = "../data/test_a/sentiment_analysis_testa.csv"
+# 测试集预测结果文件存放路径
+test_data_predict_out_path = "../data/test_a/sentiment_analysis_testa_prediction.csv"
